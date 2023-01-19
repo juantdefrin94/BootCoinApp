7558c7edac2a628b72554c684e3dbee3dbbf322e
DnYAlv <dennyginting40@gmail.com>
2023-01-20 02:25:59 +0700

    nvm, the error is in visual studio (vs sucks, vscode yes)

--- BootCoinApp/Controllers/HomeController.cs
+++ BootCoinApp/Controllers/HomeController.cs
@@ -8,15 +8,18 @@ namespace BootCoinApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult AddReward()
         {
-            return View();
+            var rewards = _context.Rewards.ToList();
+            return View(rewards);
         }
 
         public IActionResult AddCoin()
