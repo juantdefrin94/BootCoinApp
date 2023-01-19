f0060421f3b99c8ad1a5de0407daa0231ddca359
sh0rky <70480858+sh0rky@users.noreply.github.com>
2023-01-19 07:01:00 +0700

    Testing Seeder

--- BootCoinApp/Program.cs
+++ BootCoinApp/Program.cs
@@ -14,6 +14,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+if(args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    Seed.SeedData(app);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
