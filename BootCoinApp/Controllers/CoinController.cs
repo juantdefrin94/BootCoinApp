6a29a0d378b8b070f64550f1bf8bb18f4ca7f085
sh0rky <70480858+sh0rky@users.noreply.github.com>
2023-02-10 22:57:28 +0700

    coin

--- BootCoinApp/Controllers/CoinController.cs
+++ BootCoinApp/Controllers/CoinController.cs
@@ -1,6 +1,8 @@
 ﻿using BootCoinApp.Data;
 using BootCoinApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BootCoinApp.Controllers
 {
@@ -23,6 +25,48 @@ namespace BootCoinApp.Controllers
             List<GroupUser> groups = _context.GroupUsers.ToList();
 
             return View(groups);
+        }
+
+        public IActionResult CoinSelect()
+        {
+            List<AddCoinCategory> coins = _context.AddCoinCategories.ToList();
+            return View(coins);
+        }
+
+        [HttpPost]
+        public IActionResult AddCoinToUser(string coins, string tempId)
+        {
+            var tempList = tempId.Split(",");
+            var coinList = coins.Split(",");
+            int tempCoins = 0;
+            List<User> users = null;
+            List<User> usersAll = _context.Users.ToList();
+            foreach (var item in coinList)
+            {
+                tempCoins += int.Parse(item);
+            }
+            foreach (var item in tempList)
+            {
+                foreach (var userTemp in usersAll)
+                {
+                    int x = int.Parse(item);
+                    if(userTemp.Id == x)
+                    {
+                        var user = _context.Users.Where(u => u.Id == x).FirstOrDefault();
+                        user.Bootcoin += tempCoins;
+                        _context.Update(user);
+                        _context.SaveChanges();
+                    }
+                }
+            }
+            return RedirectToAction("CoinPeople");
+        }
+        [HttpPost]
+        public IActionResult PassingUser(string temp)
+        {
+            //var tempList = temp.Split(",");
+            ViewData["tempList"] = temp;
+            return Redirect("CoinSelect");
         }
     }
 }
